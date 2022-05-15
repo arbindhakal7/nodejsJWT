f3d397f8026ed7809622a1c6e8ff7062a8a120a3
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-05-15 17:28:53 +0545

    :tada: db connected

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth.js");
+
+dotenv.config();
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("connected to db!")
+);
+
+app.use("/api/user", authRoute);
 
 app.listen(3000, () => {
   console.log("Server running");
