873893be4039d0bc6c445ceae3e70bd7074b6f0b
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-05-15 20:12:17 +0545

    :lipstick: register and login validation

--- validation.js
+++ validation.js
@@ -10,4 +10,13 @@ const registerValidation = () => {
   return Joi.validate(data, schema);
 };
 
+const loginValidation = () => {
+  const schema = {
+    email: Joi.string().min(7).required().email(),
+    password: Joi.string().min(5).required(),
+  };
+  return Joi.validate(data, schema);
+};
+
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
