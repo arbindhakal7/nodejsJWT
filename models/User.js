fd83b1b52fe031f9ba4b7bf0bfc2d71ec81cec75
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-05-15 17:34:31 +0545

    :sparkles: usermodel created

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    max: 100,
+    min: 7,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 100,
+    min: 7,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 100,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
