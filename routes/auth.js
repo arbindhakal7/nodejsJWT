393b1c5d1a381e6a06973718a5bdd104c260ff36
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-05-15 19:46:13 +0545

    :sparkles: validation created

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,17 @@ const router = require("express").Router();
 const User = require("../models/User.js");
 
 //Validation
-const Arbin = require("@happy/arbin");
-const schema = {
-  fullname: Arbin.string().min(7).required(),
-  email: Arbin.string().min(7).required().email(),
-  password: Arbin.string().min(5).required(),
-};
+const Joi = require("@hapi/joi");
+const schema = Joi.object({
+  fullname: Joi.string().min(7).required(),
+  email: Joi.string().min(7).required().email(),
+  password: Joi.string().min(5).required(),
+});
 
 router.post("/register", async (req, res) => {
-
+  //validate
+  const { error } = Joi.validate(req.body, schema);
+  if (error) return res.status(400).send(error.details[0].message);
   const user = new User({
     fullname: req.body.fullname,
     email: req.body.email,
